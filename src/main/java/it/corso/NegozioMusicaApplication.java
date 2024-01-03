cb494a0f3541245889454905464adae78353d58b
chiararomagnoli <chiaraerre91@gmail.com>
2024-01-03 16:35:33 +0100

    commit iniziale

---
+++ src/main/java/it/corso/NegozioMusicaApplication.java
@@ -0,0 +1,13 @@
+package it.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegozioMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegozioMusicaApplication.class, args);
+	}
+
+}
