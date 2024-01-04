7eb6c8cf41c8b985bd10a5a9108001d29c4362fc
simonaconte <simona.conte93@gmail.com>
2024-01-04 17:39:36 +0100

    Merge branch 'main' of https://github.com/chiararomagnoli/negozio_musica

---
+++ src/main/java/it/corso/service/CategoriaServiceImpl.java
@@ -0,0 +1,36 @@
+package it.corso.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.corso.dao.CategoriaDao;
+import it.corso.model.Categoria;
+
+public class CategoriaServiceImpl implements CategoriaService {
+	
+	@Autowired
+	private CategoriaDao categoriaDao;
+
+	@Override
+	public void registraCategoria(Categoria categoria) {
+		categoriaDao.save(categoria);
+
+	}
+
+	@Override
+	public Categoria getCategoriaById(int id) {
+		return categoriaDao.findById(id).get();
+	}
+
+	@Override
+	public List<Categoria> getCategorie() {
+		return (List<Categoria>) categoriaDao.findAll();
+	}
+
+	@Override
+	public void cancellaCategoria(Categoria categoria) {
+		categoriaDao.delete(categoria);
+	}
+
+}
