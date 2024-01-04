25babcabd64a4cbf8e2b6cd91c9729f1518b11cd
simonaconte <simona.conte93@gmail.com>
2024-01-04 15:39:54 +0100

    Categoria.java package model

---
+++ src/main/java/it/corso/model/Categoria.java
@@ -0,0 +1,37 @@
+package it.corso.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="categorie")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id_categoria;
+	
+	@Column(name="nome_categoria")
+	private String nome_categoria;
+	
+	
+	public int getId_categoria() {
+		return id_categoria;
+	}
+	public void setId_categoria(int id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+	public String getNome_categoria() {
+		return nome_categoria;
+	}
+	public void setNome_categoria(String nome_categoria) {
+		this.nome_categoria = nome_categoria;
+	}
+	
+	
+
+}
