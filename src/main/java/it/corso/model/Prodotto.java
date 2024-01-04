fbd9bb125b0ff0ac2b2c729a0871ab509653c71f
chiararomagnoli <chiaraerre91@gmail.com>
2024-01-04 16:48:59 +0100

    getter/setter di Prodotto.java

--- src/main/java/it/corso/model/Prodotto.java
+++ src/main/java/it/corso/model/Prodotto.java
@@ -13,6 +13,106 @@ import jakarta.persistence.Table;
 @Entity // la classe rappresenta un'entità persistente, ovvero un oggetto che può essere memorizzato in un database
 @Table(name = "prodotti") // specifica il nome della tabella associata
 public class Prodotto {
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNome_prodotto() {
+		return nome_prodotto;
+	}
+
+
+	public void setNome_prodotto(String nome_prodotto) {
+		this.nome_prodotto = nome_prodotto;
+	}
+
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+
+	public int getDisponibilita() {
+		return disponibilita;
+	}
+
+
+	public void setDisponibilita(int disponibilita) {
+		this.disponibilita = disponibilita;
+	}
+
+
+	public double getSconto() {
+		return sconto;
+	}
+
+
+	public void setSconto(double sconto) {
+		this.sconto = sconto;
+	}
+
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+
+	public Sottocategoria getSottocategoria() {
+		return sottocategoria;
+	}
+
+
+	public void setSottocategoria(Sottocategoria sottocategoria) {
+		this.sottocategoria = sottocategoria;
+	}
+
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+
+	public String getCondizione() {
+		return condizione;
+	}
+
+
+	public void setCondizione(String condizione) {
+		this.condizione = condizione;
+	}
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
