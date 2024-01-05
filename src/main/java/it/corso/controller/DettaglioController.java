9e9881c5f3ad63f7d44dbfe2955945b2e349d766
nbodysimulation <simona.conte93@gmail.com>
2024-01-05 17:04:02 +0100

    DettaglioController

--- src/main/java/it/corso/controller/DettaglioController.java
+++ src/main/java/it/corso/controller/DettaglioController.java
@@ -1,17 +1,28 @@
 package it.corso.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import it.corso.model.Prodotto;
+import it.corso.service.ProdottoService;
+
 @Controller
 @RequestMapping("/dettaglio")
 public class DettaglioController {
 	
+	@Autowired
+	private ProdottoService prodottoService;
+	
 	@GetMapping
 	public String getPage(@RequestParam(name="id", required=true) int id, Model model) {
+		List<Prodotto> prodotti = prodottoService.getProdotti();
+		model.addAttribute("prodotto", prodotti.get(id-1));
 		return "dettaglio";
 	}
 
