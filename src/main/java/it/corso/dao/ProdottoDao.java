437e4b6c215ab590b3ebd0011a19cdf783d7f271
nbodysimulation <simona.conte93@gmail.com>
2024-01-06 15:24:55 +0100

    ProdottoDao: modifica di findByNomeProdottoContaining con @Query////formprova.html: modifica del form di ricerca

--- src/main/java/it/corso/dao/ProdottoDao.java
+++ src/main/java/it/corso/dao/ProdottoDao.java
@@ -2,7 +2,9 @@ package it.corso.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import it.corso.model.Prodotto;
 
@@ -16,6 +18,7 @@ public interface ProdottoDao extends CrudRepository<Prodotto, Integer> {
 	List<Prodotto> findAllBySconto(double sconto);
 	
 	//metodi utili per il form di ricerca
-	List<Prodotto> findByNomeProdottoContaining(String nome);
+	@Query (value="SELECT * FROM prodotti WHERE nome_prodotto LIKE '%nome%';", nativeQuery = true)
+	List<Prodotto> findByNomeProdottoContaining(@Param("nome") String nome);
 
 }
