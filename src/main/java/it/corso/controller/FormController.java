7654229a0aaaee48160672c8741c073267f9bd6d
nbodysimulation <simona.conte93@gmail.com>
2024-01-06 22:52:42 +0100

    ricerca per nome nel form sistemata in formprova/ ProdottoDao sistamto con aggiunta dei service per categoria e sottocategoria/ in Form controller è stata tola l'annotation @Query

--- src/main/java/it/corso/controller/FormController.java
+++ src/main/java/it/corso/controller/FormController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import it.corso.model.Categoria;
 import it.corso.model.Prodotto;
+import it.corso.model.Sottocategoria;
+import it.corso.service.CategoriaService;
 import it.corso.service.ProdottoService;
+import it.corso.service.SottocategoriaService;
 
 @Controller
 @RequestMapping("/form")
@@ -19,10 +23,20 @@ public class FormController {
 	@Autowired
 	private ProdottoService prodottoService;
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@Autowired
+	private SottocategoriaService sottocategoriaService;
+	
 	@GetMapping
 	public String getPage(Model model) {
 		List<Prodotto> prodotti = prodottoService.getProdotti();
 		model.addAttribute("prodotti", prodotti);
+		List<Categoria> categorie = categoriaService.getCategorie();
+		model.addAttribute("categorie", categorie);
+		List<Sottocategoria> sottocategorie = sottocategoriaService.getSottocategoria();
+		model.addAttribute("sottocategorie", sottocategorie);
 		return "formprova";
 	}	
 	
