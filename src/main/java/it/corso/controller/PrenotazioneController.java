49ce7c6ce72ff0c0ed154967c371774aee297263
nbodysimulation <simona.conte93@gmail.com>
2024-01-10 10:53:00 +0100

    Modifica PrenotazioneController con aggiunta del model e attributo service

--- src/main/java/it/corso/controller/PrenotazioneController.java
+++ src/main/java/it/corso/controller/PrenotazioneController.java
@@ -1,15 +1,25 @@
 package it.corso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.corso.model.Utente;
+import it.corso.service.UtenteService;
 
 @Controller
 @RequestMapping("/prenotazione")
 public class PrenotazioneController {
+	
+	@Autowired
+	UtenteService utenteService;
 
 	@GetMapping
-	public String  visualizzaFormPrenotazione() {
+	public String  visualizzaFormPrenotazione(Model model) {
+		Utente utente = new Utente();
+		model.addAttribute("utente", utente);
 		return "contattaci";
 	}
 }
