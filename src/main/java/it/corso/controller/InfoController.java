f856f2656afbeceaa0a3cd678a9252a12aa05ac8
chiararomagnoli <chiaraerre91@gmail.com>
2024-01-10 15:33:13 +0100

    InfoController e Thymeleaf di info.html

---
+++ src/main/java/it/corso/controller/InfoController.java
@@ -0,0 +1,15 @@
+package it.corso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/info")
+public class InfoController {
+	@GetMapping
+	public String getPage(Model model) {
+		return "info";
+	}
+}
