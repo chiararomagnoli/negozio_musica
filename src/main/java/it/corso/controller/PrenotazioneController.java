33a7b651c0ff1c5de90f4524367d6552b0b6f0a3
nbodysimulation <simona.conte93@gmail.com>
2024-01-10 10:56:42 +0100

    PrenotazioneController | aggiunto PostMapping per il form

--- src/main/java/it/corso/controller/PrenotazioneController.java
+++ src/main/java/it/corso/controller/PrenotazioneController.java
@@ -3,11 +3,15 @@ package it.corso.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.corso.model.Utente;
 import it.corso.service.UtenteService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/prenotazione")
@@ -21,5 +25,17 @@ public class PrenotazioneController {
 		Utente utente = new Utente();
 		model.addAttribute("utente", utente);
 		return "contattaci";
+	}
+	
+	@PostMapping
+	public String formManager(@Valid @ModelAttribute("utente") Utente utente,
+			BindingResult result) {
+		if(result.hasErrors())
+			return "contattaci";		
+		utenteService.registraPrenotazioneUtente(utente);
+		
+		return "redirect:/prenotazione";
+		
+		
 	}
 }
