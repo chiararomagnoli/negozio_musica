4da325e1cc982e6213d9dc90ba9747eac811ba5e
Fla-Ser <flaviaserino99@gmail.com>
2024-01-10 10:19:53 +0100

    PrenotazioneController implementato

---
+++ src/main/java/it/corso/controller/PrenotazioneController.java
@@ -0,0 +1,15 @@
+package it.corso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/prenotazione")
+public class PrenotazioneController {
+
+	@GetMapping
+	public String  visualizzaFormPrenotazione() {
+		return "contattaci";
+	}
+}
