715b2e162812bf05f501a35e4ffe71107422c88a
nbodysimulation <simona.conte93@gmail.com>
2024-01-10 16:09:54 +0100

    aggiunta model in DettaglioController

--- src/main/java/it/corso/controller/DettaglioController.java
+++ src/main/java/it/corso/controller/DettaglioController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.corso.model.Prodotto;
+import it.corso.service.CategoriaService;
 import it.corso.service.ProdottoService;
+import it.corso.service.SottocategoriaService;
 
 @Controller
 @RequestMapping("/dettaglio")
@@ -19,11 +21,19 @@ public class DettaglioController {
 	@Autowired
 	private ProdottoService prodottoService;
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@Autowired
+	private SottocategoriaService sottocategoriaService;
+	
 	
 	@GetMapping
 	public String getPage(@RequestParam(name="id", required=true) int id, Model model) {
 		List<Prodotto> prodotti = prodottoService.getProdotti();
 		model.addAttribute("prodotto", prodotti.get(id-1));
+		model.addAttribute("categorie", categoriaService.getCategorie());
+		model.addAttribute("sottocategorie", sottocategoriaService.getSottocategoria());
 		return "dettaglio";
 	}
 
