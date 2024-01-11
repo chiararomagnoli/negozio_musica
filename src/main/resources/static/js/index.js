61d7668d702c9c8bc72841167ceb1e3f65f166f1
chiararomagnoli <chiaraerre91@gmail.com>
2024-01-11 09:25:05 +0100

    frecce carosello (NON TERMINATE)

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -58,6 +58,22 @@
   });
 });
 
+        $('.owl-carousel').owlCarousel({
+            margin: 10,
+            nav: true,
+            navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 3
+                }
+            }
+        });
 
 // ---- FINE HOMEPAGE -----
 
