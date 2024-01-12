57ca6ddd7e4657b174b54b31107514d940a6ec31
nbodysimulation <simona.conte93@gmail.com>
2024-01-12 16:57:42 +0100

    contattaci.html da finire

--- src/main/java/it/corso/controller/PrenotazioneController.java
+++ src/main/java/it/corso/controller/PrenotazioneController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.corso.model.Utente;
 import it.corso.service.CategoriaService;
+import it.corso.service.ProdottoService;
 import it.corso.service.SottocategoriaService;
 import it.corso.service.UtenteService;
 import jakarta.validation.Valid;
@@ -33,9 +35,13 @@ public class PrenotazioneController {
 	@Autowired
 	SottocategoriaService sottocategoriaService;
 	
+	@Autowired
+	ProdottoService prodottoService;
+	
 
 	@GetMapping
-	public String getPage(Model model, @RequestParam(name="id", required=false) Integer idProdotto, 
+	public String getPage(Model model, 
+			@RequestParam(name="id", required=false) Integer idProdotto, 
 			@RequestParam(name="nome", required=false) String nome, 
 			@RequestParam(name="cognome", required = false) String cognome) {
 		//model utili per la navbar
@@ -50,39 +56,37 @@ public class PrenotazioneController {
 		if(nome==null&&cognome==null) {
 			model.addAttribute("appointmentReceived", false);
 			model.addAttribute("utente", new Utente());
-			
+			model.addAttribute("id", idProdotto==null? 0:idProdotto.intValue());
+			model.addAttribute("prodotto", prodottoService.getProdottoById(idProdotto==null? 0:idProdotto.intValue()));
 		}else {
 			Utente utente=new Utente();
 			utente.setNome(nome);
 			utente.setCognome(cognome);
 			model.addAttribute("appointmentReceived", true);
 			model.addAttribute("utente", utente);
+			model.addAttribute("id", idProdotto==null? 0:idProdotto.intValue());
+			model.addAttribute("prodotto", prodottoService.getProdottoById(idProdotto));
 			// Genera un orario casuale a intervalli di 30 minuti tra le 9:00 e le 17:30
 	        LocalTime orarioCasuale = generaOrarioCasuale();
 
 	        // Passa l'orario casuale al modello
 	        model.addAttribute("orarioCasuale", orarioCasuale);
 		}
-
-		
-		model.addAttribute("id", idProdotto==null? 0:idProdotto.intValue());
-		
-		
+				
 		return "contattaci";
 	}
-
+//RequestParam(name="id") Integer idProdotto,|+
 	@PostMapping
 	public String formManager(@Valid @ModelAttribute("utente") Utente utente,
-			
-			BindingResult result, Model model) {
+			BindingResult result, Model model,  RedirectAttributes redirectAttributes) {
+		
 		
 		if(result.hasErrors()) {
 			return "contattaci";}else{	
 		utenteService.registraPrenotazioneUtente(utente);
 		model.addAttribute("appointmentReceived", true);
-        System.out.println("Form compiled successfully. Setting appointmentReceived to true.");
-
-		return "redirect:/prenotazione?nome="+utente.getNome()+"&cognome="+utente.getCognome();
+        System.out.println("Form compiled successfully. Setting appointmentReceived to true."); //controllo
+   		return "redirect:prenotazione?nome="+utente.getNome()+"&cognome="+utente.getCognome();
 			}
 	}
 	
