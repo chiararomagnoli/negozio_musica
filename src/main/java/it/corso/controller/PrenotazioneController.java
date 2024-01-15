338469800269215009da144a6982c0e6a07a2a66
nbodysimulation <simona.conte93@gmail.com>
2024-01-15 01:10:00 +0100

    tolti import inutili

--- src/main/java/it/corso/controller/PrenotazioneController.java
+++ src/main/java/it/corso/controller/PrenotazioneController.java
@@ -7,20 +7,17 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.corso.model.Utente;
 import it.corso.service.CategoriaService;
 import it.corso.service.ProdottoService;
 import it.corso.service.SottocategoriaService;
 import it.corso.service.UtenteService;
-import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/prenotazione")
